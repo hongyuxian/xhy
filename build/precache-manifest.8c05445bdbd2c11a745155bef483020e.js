0db71cd63827565b84337d77de419f8268dcbd10
hongyuxian <625390591@qq.com>
2019-07-29 17:12:45 +0800

    asd

---
+++ build/precache-manifest.8c05445bdbd2c11a745155bef483020e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86120b2a354a0011e83f5f35e884dac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "657cc4c37bc40c88026d",
+    "url": "/static/css/2.c9c425ca.chunk.css"
+  },
+  {
+    "revision": "279a6b99273b0468f3ba",
+    "url": "/static/css/main.02c6d431.chunk.css"
+  },
+  {
+    "revision": "657cc4c37bc40c88026d",
+    "url": "/static/js/2.8913a07c.chunk.js"
+  },
+  {
+    "revision": "279a6b99273b0468f3ba",
+    "url": "/static/js/main.7d0adc0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
